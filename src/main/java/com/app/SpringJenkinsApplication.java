02dd5f6feb0acb1b62bab3cbc376403902490d32
mvvramana <srinumedisetti35@gmail.com>
2025-03-04 16:47:22 +0530

    second commit

--- src/main/java/com/app/SpringJenkinsApplication.java
+++ src/main/java/com/app/SpringJenkinsApplication.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
-	
+
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
-	
+
+	@PostConstruct
 	public void init() {
 		logger.info("Application started...");
 	}
