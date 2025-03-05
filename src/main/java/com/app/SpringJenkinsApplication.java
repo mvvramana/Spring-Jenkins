5a7120ce23b98f19c980b649043a4ce012943a3e
mvvramana <srinumedisetti35@gmail.com>
2025-03-05 15:25:18 +0530

    first commit

--- src/main/java/com/app/SpringJenkinsApplication.java
+++ src/main/java/com/app/SpringJenkinsApplication.java
@@ -14,12 +14,12 @@ public class SpringJenkinsApplication {
 
 	@PostConstruct
 	public void init() {
-		logger.info("Application started...");
+		logger.iinfo("Application started...");
 		logger.info("Application ended...");
 	}
 
 	public static void main(String[] args) {
-		logger.info("Application executed...");
+		logger.info("------------Application executed------------");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
